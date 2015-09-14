0757637871d4026dcc7511c8a3af56c86d25db7c
alancerio18 <alancerio18@alex>
2015-09-14 12:02:34 -0500

    Has it set to where you input a letter and it outputs what kind of phone you have.

--- src/abstractions/Phone.java
+++ src/abstractions/Phone.java
@@ -4,11 +4,31 @@
  * and open the template in the editor.
  */
 package abstractions;
-
+ 
+import java.util.Scanner;
 /**
  *
  * @author alancerio18
  */
 public class Phone {
+    public static void main(String[] args) {
+     Scanner keyBoard = new Scanner(System.in);
+      
+     System.out.println("Enter in a G,O, or and H");
+        char input = keyBoard.next(".").charAt(0);
+        
+       Galaxys6 galaxy = new Galaxys6("Galaxys6");
+       HtcOne  htc = new HtcOne("HTC One");
+       OneplusOne one = new OneplusOne("OnePlus One");
+       
+        if(input == 'G' ){
+            System.out.println("You put in " + input + " " + "so you have the " + galaxy.getGalaxy());
+        }else if(input == 'H'){
+        System.out.println("You put in " + input + " " + "so you have the " + htc.getHtc());
+    }else if (input == 'O'){
+        System.out.println("You put in " + input + " " + "so you have the " + one.getOneplusOne());
+    }
+        
+    }
     
 }
